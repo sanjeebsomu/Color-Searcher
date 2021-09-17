1d97039a38f18268483557b076aaea4ee7289d9a
sanjeebsomu <83944578+sanjeebsomu@users.noreply.github.com>
2021-09-17 22:53:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function search(){
     //here the provided class name should be same in every element, in which we wanna perform array.from-for each method
     let colorName = document.getElementsByClassName('colorName')
 
-    //array.from-for each will perform provided function once for each array element.
+    //array.from-for each will perform provided function once for each array element. This will run one by one in every 
+Color elements, it will go to the 1st element, search for the releavant text and so on.
     Array.from(colorName).forEach(function(element){
 
         //grabbing color name(text) from span. [0] means first element of this tag.
